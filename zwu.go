6be5f1fa928f9cede2cf1d6e212f0a434440412e
sky96111 <sky96111@outlook.com>
2023-02-16 15:46:52 +0800

    refactor: reuse client resources

--- zwu.go
+++ zwu.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
@@ -11,36 +12,45 @@ import (
 	"strings"
 )
 
+var client = http.DefaultClient
+
 func main() {
 	var (
 		username string
 		password string
-		isLogout bool
-		isStatus bool
 	)
-	var client = http.DefaultClient
 
-	flag.StringVar(&username, "u", "zc1", "Username")
-	flag.StringVar(&password, "p", "1", "Password")
-	flag.BoolVar(&isLogout, "L", false, "Logout")
-	flag.BoolVar(&isStatus, "S", false, "Status")
+	flag.StringVar(&username, "u", "", "Username")
+	flag.StringVar(&password, "p", "", "Password")
+	logoutOpt := flag.Bool("L", false, "Perform Logout operation")
+	statusOpt := flag.Bool("S", false, "Perform GetStatus operation")
 	flag.Parse()
 
-	if isLogout && isStatus {
-		log.Fatalln("more than ONE operation")
+	if *logoutOpt {
+		err := logout()
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println("success")
+		}
+	} else if *statusOpt {
+		err := status()
+		if err != nil {
+			log.Println(err)
+		}
+	} else if username == "" || password == "" {
+		log.Println("username and password can not be empty")
+	} else {
+		err := login(username, password)
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println("success")
+		}
 	}
-	if isLogout {
-		logout(client)
-		return
-	}
-	if isStatus {
-		status(client)
-		return
-	}
-	login(username, password, client)
 }
 
-func login(username string, password string, client *http.Client) {
+func login(username string, password string) error {
 	log.Println("login as", username, "...")
 
 	var data = strings.NewReader(`DDDDD=` +
@@ -50,94 +60,106 @@ func login(username string, password string, client *http.Client) {
 		`&0MKKey=%B5%C7%C2%BC+Login`)
 	req, err := http.NewRequest("POST", "http://192.168.255.19/0.htm", data)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 	}(resp.Body)
 	newReader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
 	bodyText, err := io.ReadAll(newReader)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if strings.Contains(string(bodyText), "You have successfully logged into our system.") {
-		log.Println("success")
+		return nil
 	} else if strings.Contains(string(bodyText), "msga='userid error1'") {
-		log.Fatalln("username", username, "not exist")
+		return errors.New("username " + username + " not exist")
+	} else if strings.Contains(string(bodyText), "msga='userid error2'") {
+		return errors.New("this account is banned")
 	} else if strings.Contains(string(bodyText), "msga='ldap auth error'") {
-		log.Fatalln("username or password error")
+		return errors.New("username or password error")
 	} else if strings.Contains(string(bodyText), "msga='[02], 本帐号只能在指定 IP 段使用'") {
-		log.Fatalln("this account only available in specified IP range")
+		return errors.New("this account only available in specified IP range")
 	} else {
-		log.Fatalln("unknown error")
+		return errors.New("unknown error")
 	}
 }
 
-func logout(client *http.Client) {
+func logout() error {
 	log.Println("logout...")
 
 	req, err := http.NewRequest("GET", "http://192.168.255.19/F.htm", nil)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 	}(resp.Body)
 	newReader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
 	bodyText, err := io.ReadAll(newReader)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if strings.Contains(string(bodyText), "Msg=14") {
-		log.Println("success")
+		return nil
 	} else {
-		log.Fatalln("unknown error")
+		return errors.New("unknown error")
 	}
 }
 
-func status(client *http.Client) {
+func getInfo() (string, error) {
 	req, err := http.NewRequest("GET", "http://192.168.255.19/", nil)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 	}(resp.Body)
 	newReader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
 	bodyText, err := io.ReadAll(newReader)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	if strings.Contains(string(bodyText), `location.href="0.htm"`) {
-		log.Fatalln("device is not authed")
+		return string(bodyText), errors.New("device is not authed")
+	} else {
+		return string(bodyText), nil
 	}
-	time := strings.Split(strings.Split(string(bodyText), "time='")[1], "';")[0]
-	flow := strings.Split(strings.Split(string(bodyText), "flow='")[1], "';")[0]
+}
+
+func status() error {
+	bodyText, err := getInfo()
+	if err != nil {
+		return err
+	}
+
+	time := strings.Split(strings.Split(bodyText, "time='")[1], "';")[0]
+	flow := strings.Split(strings.Split(bodyText, "flow='")[1], "';")[0]
 	time = strings.Trim(time, " ")
 	flow = strings.Trim(flow, " ")
 
@@ -145,4 +167,5 @@ func status(client *http.Client) {
 	MB /= 1024
 	log.Println("time:", time, "Min")
 	log.Println("flow:", MB, "MB")
+	return nil
 }
