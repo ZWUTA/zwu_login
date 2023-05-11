4210e1388452ccbd45fe41986f234b96880d6e22
sky96111 <sky96111@outlook.com>
2023-05-11 21:23:41 +0800

    refactor: making project more modular

--- utils/Login.go
+++ utils/Login.go
@@ -31,6 +31,7 @@ func Login(client *http.Client, username string, password string) error {
 
 	switch {
 	case strings.Contains(bodyText, "You have successfully logged into our system."):
+		log.Println("login success")
 		return nil
 	case strings.Contains(bodyText, "msga='userid error1'"):
 		return errors.New("username " + username + " not exist")
@@ -43,4 +44,26 @@ func Login(client *http.Client, username string, password string) error {
 	default:
 		return errors.New("unknown error")
 	}
+}
+
+func LoginFromConfig(client *http.Client) error {
+	users, err := LoadUsers()
+	if err != nil {
+		return err
+	}
+
+	if len(users) == 0 {
+		return errors.New("no user found")
+	}
+
+	log.Println("found", len(users), "user(s)")
+
+	for _, user := range users {
+		if err := Login(client, user.Username, user.Password); err == nil {
+			return nil
+		}
+		log.Println("login failed")
+	}
+
+	return errors.New("all login attempts failed")
 }
--- zwu.go
+++ zwu.go
@@ -30,81 +30,29 @@ func init() {
 }
 
 func main() {
-	var err error
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	client := http.DefaultClient
 
-	switch {
-	case optLogout:
-		err = utils.Logout(client)
-		if err != nil {
-			log.Println(err)
-		}
-	case optStatus:
-		err = utils.Status(client)
-		if err != nil {
-			log.Println(err)
-		}
-	case optCreateConfig:
-		err = utils.CreateConfig()
-		if err != nil {
-			log.Println(err)
-		}
-	default:
-		if username != "" && password != "" {
-
-			_, _, err = utils.GetAccount(client)
-			if err == nil && !optForceReLogin {
-				log.Println("skip: device already authed")
-				log.Println(`if you really want to login again, retry with "-f" arg`)
-				return
-			}
-
-			err = utils.Login(client, username, password)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			log.Println("login successful")
-			return
-		}
-
-		log.Println("username and password not provided")
-		log.Println("try parsing from config file...")
-
-		users, err := utils.LoadUsers()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		if len(users) == 0 {
-			log.Println("no user found")
-			return
-		}
-		log.Println("found", len(users), "user(s)")
-
-		for _, user := range users {
-			_, _, err = utils.GetAccount(client)
-			if err == nil && !optForceReLogin {
-				log.Println("skip: device already authed")
-				log.Println(`if you really want to login again, retry with "-f" arg`)
-				return
-			}
-
-			err = utils.Login(client, user.Username, user.Password)
-			if err != nil {
-				log.Println("login failed")
-				log.Println(err)
-				continue
-			}
-			_, _, err = utils.GetAccount(client)
-			if err == nil {
-				log.Println("login successful")
-				return
-			}
-		}
-
-		log.Println("all login attempts failed")
+	if optLogout {
+		return utils.Logout(client)
 	}
+
+	if optStatus {
+		return utils.Status(client)
+	}
+
+	if optCreateConfig {
+		return utils.CreateConfig()
+	}
+
+	if username == "" || password == "" {
+		return utils.LoginFromConfig(client)
+	}
+
+	return utils.Login(client, username, password)
 }
